928e117e2a8f9dc9a3f487969b81b4e714af333b
manit <ermanit007@gmail.com>
2017-09-25 10:43:02 +0530

    initial commit

---
+++ app/src/main/java/com/grademojo/internal_database/Main5Activity.java
@@ -0,0 +1,17 @@
+package com.grademojo.internal_database;
+
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.VideoView;
+
+public class Main5Activity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.scroll_view);
+
+    }
+}
